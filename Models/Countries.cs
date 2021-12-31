07e306988dd354e108467c6162eb83495160203a
Mahmoud <massar44980@gmail.com>
2021-12-31 14:53:52 +0400

    not null name

--- Models/Countries.cs
+++ Models/Countries.cs
@@ -1,8 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace School.Models
 {
     public class Countries
     {
+        public Countries(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+
+        }
         public int id { get; set; }
+
         public string name { get; set; }
     }
 }
