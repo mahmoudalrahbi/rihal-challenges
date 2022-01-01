f6f8343f4de52fdd0e555b6b1bc7c6ae48bfe7ac
Mahmoud <massar44980@gmail.com>
2022-01-01 12:24:58 +0400

    add migration

---
+++ Migrations/20220101082349_BaseEntityAdded.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace School.Migrations
+{
+    public partial class BaseEntityAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Classes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    class_name = table.Column<string>(type: "TEXT", nullable: true),
+                    created_date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    modified_date = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Classes", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", nullable: true),
+                    created_date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    modified_date = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", nullable: true),
+                    date_of_birth = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    class_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    country_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    created_date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    modified_date = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Students_Classes_class_id",
+                        column: x => x.class_id,
+                        principalTable: "Classes",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Students_Countries_country_id",
+                        column: x => x.country_id,
+                        principalTable: "Countries",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_class_id",
+                table: "Students",
+                column: "class_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_country_id",
+                table: "Students",
+                column: "country_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Classes");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
